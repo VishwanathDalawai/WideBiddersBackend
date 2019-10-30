884bf99620e7cf3e8d63aa1e804dd60f8a816dc1
sahanais <sahanakhushi28@gmail.com>
2019-10-30 23:34:23 +0530

    angular

--- Auction/src/app/app.module.ts
+++ Auction/src/app/app.module.ts
@@ -26,9 +26,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
  
 import { ToastrModule } from 'ngx-toastr';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
- 
-import { ToastrModule } from 'ngx-toastr';
+
 
 
 @NgModule({
