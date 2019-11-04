211a01b937796ddfaa320853e8d030d6973e785d
Vishwanathpd <vishwanthdalawai321@gmail.com>
2019-11-04 10:59:41 +0530

    added context path in application.propperties

--- WideBiddersBackend/src/main/java/com/widebidders/models/db/AuctionTransactionDaoImpl.java
+++ WideBiddersBackend/src/main/java/com/widebidders/models/db/AuctionTransactionDaoImpl.java
@@ -106,7 +106,6 @@ public class AuctionTransactionDaoImpl implements AuctionTransactionDao {
 					bid.setAuctionMaster(auctionMaster);
 					break;
 				}
-				
 				logger.info("Auction id for the product is" + auctionMaster.getAuctionId());
 			}
 			Customer bidderCustomer = customerDaoImpl.getCustomerById(customerId);
@@ -241,5 +240,4 @@ public class AuctionTransactionDaoImpl implements AuctionTransactionDao {
 		}
 		return null;
 	}
-
 }
